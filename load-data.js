6aac488e80987797fa733aea544d28bd2d52f627
Joshua Paulsen <jpaulsen3110@gmail.com>
2018-06-25 10:27:38 -0400

    Installed npm modules and added to LOAD-DATA.JS

--- load-data.js
+++ load-data.js
@@ -2,7 +2,7 @@ require("dotenv").config()
 const PouchDB = require("pouchdb-core")
 PouchDB.plugin(require("pouchdb-adapter-http"))
 
-const JPArtDB = new PouchDB(
+const db = new PouchDB(
   `${process.env.COUCH_HOSTNAME}${process.env.COUCH_DBNAME}`
 )
 
