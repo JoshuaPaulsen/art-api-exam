1c31262c012edb40c82457e89d8f16d59d6f4ad7
Joshua Paulsen <jpaulsen3110@gmail.com>
2018-06-25 10:40:21 -0400

    Declared variables at the top of app.js

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
+require("dotenv").config()
+const express = require("express")
+const api = express("express")
+const port = process.env.PORT || 5000
+const bodyParser = require("body-parser")
+const NodeHTTPError = require("node-http-error")
+const {} = require("ramda")
+const requiredFieldsChecker =
+  //const { } = require("./dal")
 
-app.get('/', function(req, res, next) {
-  res.send('Welcome to the Art API. Manage all the paintings.')
+  api.use(bodyParser.json())
+
+api.get("/", function(req, res, next) {
+  res.send("Welcome to the Art API. Manage all the paintings.")
 })
